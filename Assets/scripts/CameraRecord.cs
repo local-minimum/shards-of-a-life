714a9de4556e70db01dd2fad55f3286c2e20b21a
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-11-08 19:48:34 +0100

    Improved editor exposed parameters and doing multiple screen captures

--- Assets/scripts/CameraRecord.cs
+++ Assets/scripts/CameraRecord.cs
@@ -3,39 +3,43 @@ using System.Collections.Generic;
 
 public class CameraRecord : MonoBehaviour {
 
-	public string fileName = "capture_{0}.jpg";
+	public string fileName = "capture_{0}_{1}.jpg";
 	public int leadingZeros = 3;
-	public string recordKey = "r";
+	public KeyCode recordKey = KeyCode.C;
 	public float fps = 14f;
 
+	private int recording = 0;
 	private string intFormat;
 	private int curFrame = 0;
-	private bool recording = false;
+	private bool isRecording = false;
 
 	// Use this for initialization
 	void Start () {
 		intFormat = new string('0', leadingZeros);
-
+		while (System.IO.File.Exists(string.Format(fileName, recording, curFrame.ToString(intFormat))))
+			recording++;
+		Debug.Log(string.Format("Will start at recording {0}", recording));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(recordKey)) {
-			if (!recording) {
-				Debug.Log("Recording");
-				recording = true;
+			if (!isRecording) {
+				Debug.Log(string.Format("Recording {0}", recording));
+				isRecording = true;
 				StartCoroutine(record());
 			} else {
 				Debug.Log("Recording complete");
-				recording = false;
+				isRecording = false;
 				curFrame = 0;
+				recording ++;
 			}
 		}
 	}
 
 	IEnumerator<WaitForSeconds> record() {
-		while (recording) {
-			Application.CaptureScreenshot(string.Format(fileName, curFrame.ToString(intFormat)));
+		while (isRecording) {
+			Application.CaptureScreenshot(string.Format(fileName, recording, curFrame.ToString(intFormat)));
 			curFrame ++;
 			yield return new WaitForSeconds(1f / fps);
 		}
